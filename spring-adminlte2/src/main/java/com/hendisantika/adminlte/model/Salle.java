5297c487369b8fc97a3acb7187cdbf1854eaaae2
5moun <101862601+5moun@users.noreply.github.com>
2023-05-02 06:31:14 +0000

    I added the class Salle

---
+++ spring-adminlte2/src/main/java/com/hendisantika/adminlte/model/Salle.java
@@ -0,0 +1,39 @@
+package com.hendisantika.adminlte.model;
+
+import java.sql.Timestamp;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToOne;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Salle extends AbstractModel<Long> {
+
+    private static final long serialVersionUID = 1L;
+
+    @Column(name = "added_date", nullable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    private Timestamp added_date;
+    
+    @Column(nullable = false, length = 50)
+	private int capacite;
+    
+    @Column(nullable = false, length = 50)
+	private int numero;
+    
+    @OneToOne(mappedBy="salle")
+    private Seance seance;
+    
+}
