9170c89812079ddc0052141eb051ec9fb7ef694a
5moun <101862601+5moun@users.noreply.github.com>
2023-05-03 22:53:27 +0000

    I added :
    OpenAppiConfig which is a Spring configuration class that creates a bean
    of type OpenAPI that is used for creating and customizing OpenAPI (
    Swagger) documentation for the eCine API.

---
+++ spring-adminlte2/src/main/java/com/hendisantika/adminlte/config/OpenApiConfig.java
@@ -0,0 +1,18 @@
+package com.hendisantika.adminlte.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI usersMicroserviceOpenAPI() {
+        return new OpenAPI().info(new Info().title("eCine API Documentation")
+                                            .description("All you need to interact with eCine")
+                                            .version("1.1"));
+    }
+}
